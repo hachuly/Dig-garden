27ab445ab76d618dadfff75d2f00e3d6e57dfecd
logbeam <desu_taratyann@yahoo.co.jp>
2017-02-24 20:56:13 +0900

    2017/02/24 地層パネルの生成方法がオブジェクト指向プログラミングになっていないところを訂正

--- Assets/Script/StatumGenerator.cs
+++ Assets/Script/StatumGenerator.cs
@@ -1,7 +1,9 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class StatumGenerator : Blueprint {
+
+    private GameObject obj;
 
 	// Use this for initialization
 	void Start () {
@@ -9,20 +11,40 @@ public class StatumGenerator : Blueprint {
 	}
 
     private void startGenerate(bool trigger){
-        if(trigger){
-            generateRandom(getResource(), getBasicPosition());
-        }else{
-            generateGeneral(getResource(), getBasicPosition());
+        for(int lhs = 0; lhs < 7; lhs++){
+            for(int rhs  = 0; rhs < 6; rhs++){
+                if(trigger){
+                    generateRandom((float)lhs, (float)rhs);
+                }else{
+                    generateGeneral((float)lhs, (float)rhs);
+                }
+            }
+
         }
 
     }
 
-    private void generateRandom(GameObject obj, Vector3 position){
+    private void generateRandom(float lhs, float rhs){
+        if(Random.Range(0, 2) == 1){
+            this.obj = Instantiate(getResource()) as GameObject;
+            this.obj.transform.position = findPosition(getBasicPosition(), lhs, rhs);
+        }
 
     }
 
-    private void generateGeneral(GameObject obj, Vector3 position){
-
+    private void generateGeneral(float lhs, float rhs){
+        this.obj = Instantiate(getResource()) as GameObject;
+        this.obj.transform.position = findPosition(getBasicPosition(), lhs, rhs);
     }
+
+    private Vector3 findPosition(Vector3 position, float lhs, float rhs){
+        return new Vector3(
+            position.x + lhs * 0.03f,
+            position.y + rhs * 0.03f,
+            position.z
+        );
+    }
+
+
 
 }
